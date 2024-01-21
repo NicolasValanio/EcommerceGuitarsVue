86e45417c7636f57c60bcccdac56639355f7227e
Nicolas Valenzuela <serviciosvalenzuela1@gmail.com>
2024-01-20 21:18:29 -0500

    Fin proyecto

--- Frontend/src/components/Footer.vue
+++ Frontend/src/components/Footer.vue
@@ -8,7 +8,9 @@
     <footer class="bg-dark mt-5 py-2">
         <div class="container-xl ">
             <p class="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
-            <p class="text-white text-center fs-4">Nicolas Valenzuela&copy; </p>
+            <p class="text-white text-center fs-4">
+                <a href="https://github.com/NicolasValanio" target="_blank" rel="noopener noreferrer"> Nicolas Valenzuela&copy; </a>
+            </p>
         </div>
     </footer>
     
